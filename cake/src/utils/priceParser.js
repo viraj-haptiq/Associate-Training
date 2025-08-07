96f5362aa7ec720b9b47bf529d91924d2f0a71a5
viraj-haptiq <viraj.yanpure@haptiq.com>
2025-08-07 17:33:14 +0530

    updated priceParser.js

--- cake/src/utils/priceParser.js
+++ cake/src/utils/priceParser.js
@@ -2,6 +2,13 @@ export const parsePrice = (priceString) => {
   if (typeof priceString !== "string") {
     return 0;
   }
-  const numericString = priceString.replace(/[^0-9.]/g, "");
+
+  let numericString = priceString.replace(/[^0-9.]/g, "");
+
+  const parts = numericString.split(".");
+  if (parts.length > 1) {
+    numericString = parts[0] + "." + parts.slice(1).join("");
+  }
+
   return parseFloat(numericString) || 0;
 };
