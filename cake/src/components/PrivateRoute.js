4142638cc5d26329c1d79f15e3359a1a2bc43bc7
viraj-haptiq <viraj.yanpure@haptiq.com>
2025-08-07 20:04:55 +0530

    updated PrivateRoute.js

--- cake/src/components/PrivateRoute.js
+++ cake/src/components/PrivateRoute.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
-  const location = useLocation();
 
   if (isAuthenticated) {
     return <Outlet />;
